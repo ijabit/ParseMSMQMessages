d31b56c87ed80705b6bf0179fac538175286d9fa
Jim Brown <jim.b@hotmail.com>
2016-03-03 12:13:18 -0500

    Added support for more types of messages

--- ErrorMessageParser/Program.cs
+++ ErrorMessageParser/Program.cs
@@ -30,16 +30,41 @@ namespace ErrorMessageParser
             var folder = new DirectoryInfo(ofd.SelectedPath);
             var files = folder.GetFiles();
             var csvBuilder = new StringBuilder();
-            csvBuilder.Append("EventDateTime\tPON\tLocationID\tID-Concat\tMessageName\tNote\tPayload\tFileName\r\n");
+            csvBuilder.Append("EventDateTime\tPON\tLocationID\tID-Concat\tMessageName\tNote\tMessageID\tPayload\tFileName\r\n");
             foreach (var file in files)
             {
                 string fullname = file.FullName;
-                var doc = XDocument.Load(file.FullName);
-                var messageName = doc.Descendants().First(d => d.Name.LocalName == "Name").Value;
-                var payload = doc.Descendants().First(d => d.Name.LocalName == "Payload").Value;
+                if (file.Length <= 0) continue;
+                var xml = File.ReadAllText(file.FullName).Replace(((char)226).ToString(), "&#150;").Replace(((char)227).ToString(), "&#151;");
+                var doc = XDocument.Parse(xml);
+
+                var messageName = doc.Descendants().FirstOrDefault(d => d.Name.LocalName == "Name")?.Value;
+                if (string.IsNullOrWhiteSpace(messageName))
+                    messageName =
+                        doc.Descendants()
+                            .FirstOrDefault(d => d.Name.LocalName == "OutboundPharmacyMessageEvent")?
+                            .Name.LocalName;
+                if (messageName == "Unhandled Mpact Connector Service Exception Notification")
+                {
+                    Console.WriteLine("Ignoring error sending notification email: " + file.FullName);
+                    continue;
+                }
+                if (doc.Root.Descendants().First().Name.LocalName == "BSSMPSRxConnectorServiceExceptionNotification")
+                {
+                    Console.WriteLine("Ignoring error sending notification email: " + file.FullName);
+                    continue;
+                }
+
+                var payload = doc.Descendants().FirstOrDefault(d => d.Name.LocalName == "Payload" | d.Name.LocalName == "MessagePayload")?.Value;
+                if (string.IsNullOrEmpty(payload))
+                {
+                    Console.WriteLine("Ignoring message with no payload: " + file.FullName);
+                    continue;
+                }
 
                 var payloadXDoc = XDocument.Parse(payload);
                 DateTime eventDate = DateTime.Parse(doc.Descendants().First(d => d.Name.LocalName == "EventDateTime").Value);
+                string firstMessageId = payloadXDoc.Descendants().First(d => d.Name.LocalName == "MessageID").Value;
 
                 string locationid = string.Empty;
                 string pon = string.Empty;
@@ -56,9 +81,14 @@ namespace ErrorMessageParser
                         locationid = payloadXDoc.Descendants("Facility").Descendants("NPI").FirstOrDefault()?.Value;
                         pon = doc.Descendants().FirstOrDefault(d => d.Name.LocalName == "OrderID")?.Value;
                         break;
+                    case "OutboundPharmacyMessageEvent":
+                        payloadNote = doc.Descendants().First(d => d.Name.LocalName == "MessageControlType")?.Value;
+                        pon = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "PrescriberOrderNumber")?.Value;
+                        locationid = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "From")?.Value;
+                        break;
                 }
 
-                csvBuilder.AppendFormat("{0}\t{1}\t{2}\t{1}-{2}\t{3}\t{4}\t{5}\t{6}\r\n", eventDate, pon, locationid, messageName, payloadNote, payload, file.FullName);
+                csvBuilder.AppendFormat("{0}\t{1}\t{2}\t{1}-{2}\t{3}\t{4}\t{5}\t{6}\t{7}\r\n", eventDate, pon, locationid, messageName, payloadNote, firstMessageId, payload, file.FullName);
             }
 
             Clipboard.SetData(DataFormats.StringFormat, csvBuilder.ToString());
