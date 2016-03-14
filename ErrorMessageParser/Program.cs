a6506d7a99230ee76813f0a6938aa0e899effc64
Jim Brown <jim.b@hotmail.com>
2016-03-14 17:57:49 -0400

    Added CSV writer library. Added parsing and output of interesting NSB header values and new types of messages.

--- ErrorMessageParser/Program.cs
+++ ErrorMessageParser/Program.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace ErrorMessageParser
 {
@@ -24,75 +24,219 @@ namespace ErrorMessageParser
             var result = ofd.ShowDialog();
             while (result != DialogResult.OK)
             {
+                if (result == DialogResult.Cancel)
+                {
+                    Console.WriteLine("Canceled. Press any key to exit.");
+                    Console.ReadKey();
+                    return;
+                }
                 result = ofd.ShowDialog();
             }
 
+            var interestingHeaders = new List<string>()
+            {
+                "NServiceBus.OriginatingMachine",
+                "NServiceBus.OriginatingEndpoint",
+                "NServiceBus.ProcessingMachine",
+                "NServiceBus.ProcessingEndpoint",
+                "NServiceBus.EnclosedMessageTypes",
+                "NServiceBus.ExceptionInfo.StackTrace"
+            };
+
             var folder = new DirectoryInfo(ofd.SelectedPath);
             var files = folder.GetFiles();
-            var csvBuilder = new StringBuilder();
-            csvBuilder.Append("EventDateTime\tPON\tLocationID\tID-Concat\tMessageName\tNote\tMessageID\tPayload\tFileName\r\n");
-            foreach (var file in files)
+
+            //var exportStringBuilder = new StringBuilder();
+            using (var exportStringBuilder = new StringWriter())
             {
-                string fullname = file.FullName;
-                if (file.Length <= 0) continue;
-                var xml = File.ReadAllText(file.FullName).Replace(((char)226).ToString(), "&#150;").Replace(((char)227).ToString(), "&#151;");
-                var doc = XDocument.Parse(xml);
+                var csv = new CsvWriter(exportStringBuilder, new CsvConfiguration() { HasHeaderRecord = true, QuoteAllFields = true, });
 
-                var messageName = doc.Descendants().FirstOrDefault(d => d.Name.LocalName == "Name")?.Value;
-                if (string.IsNullOrWhiteSpace(messageName))
-                    messageName =
-                        doc.Descendants()
-                            .FirstOrDefault(d => d.Name.LocalName == "OutboundPharmacyMessageEvent")?
-                            .Name.LocalName;
-                if (messageName == "Unhandled Mpact Connector Service Exception Notification")
+                csv.WriteField("EventDateTime");
+                csv.WriteField("PON");
+                csv.WriteField("LocationID");
+                csv.WriteField("MessageName");
+                csv.WriteField("MessageType");
+                csv.WriteField("Note");
+                csv.WriteField("DeadletterAck");
+                csv.WriteField("MessageID");
+                csv.WriteField("Payload");
+                csv.WriteField("StackTrace");
+                csv.WriteField("FileName");
+                csv.WriteField("Origin");
+                csv.WriteField("Processor");
+                csv.NextRecord();
+
+                // Todo: Add support to process ServiceControl errors?
+                Console.Write("Processing...");
+                foreach (var file in files)
                 {
-                    Console.WriteLine("Ignoring error sending notification email: " + file.FullName);
-                    continue;
+                    if (file.Length <= 0) continue;
+                    var xml =
+                        File.ReadAllText(file.FullName)
+                            .Replace(((char)226).ToString(), "&#150;")
+                            .Replace(((char)227).ToString(), "&#151;");
+                    XDocument doc = null;
+                    try
+                    {
+                        doc = XDocument.Parse(xml);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Can't parse XML content in file: " + file.FullName);
+                        continue;
+                    }
+                    if (doc.Root == null)
+                    {
+                        Console.WriteLine("Can't parse message with no content: " + file.FullName);
+                        continue;
+                    }
+                    XDocument message = null;
+                    try
+                    {
+                        message = XDocument.Parse(doc.Descendants().First(d => d.Name.LocalName == "Content").Value);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Can't parse XML inside Content element in file: " + file.FullName);
+                        continue;
+                    }
+
+                    var headers =
+                        doc.Root?.Element("Headers")
+                            .Elements("Header")
+                            .Where(d => interestingHeaders.Contains(d.Element("Name")?.Value))
+                            .ToDictionary(d => d.Element("Name")?.Value,
+                                d => d.Element("Value")?.Value);
+
+                    var messageType = headers["NServiceBus.EnclosedMessageTypes"].Split(new char[] { ',' }).First();
+                    if (messageType.StartsWith("Mps.TeRx.Exceptions.Notification.") || messageType == "Mps.TeRx.Exceptions.UnhandledMpsrxConnectorServiceRxFillCommandExceptionNotification")
+                    {
+                        Console.WriteLine("Ignoring error sending notifications: " + file.FullName);
+                        continue;
+                    }
+
+                    var payload =
+                        message.Descendants()
+                            .FirstOrDefault(d => d.Name.LocalName == "Payload" | d.Name.LocalName == "MessagePayload")?
+                            .Value;
+                    if (string.IsNullOrEmpty(payload))
+                    {
+                        Console.WriteLine("Ignoring message with no payload: " + file.FullName);
+                        continue;
+                    }
+
+                    var stackTrace = headers.ContainsKey("NServiceBus.ExceptionInfo.StackTrace")
+                        ? headers["NServiceBus.ExceptionInfo.StackTrace"]
+                        : "";
+
+                    var processingEndpoint = headers.ContainsKey("NServiceBus.ProcessingEndpoint") ? headers["NServiceBus.ProcessingEndpoint"] : "";
+                    var processingMachine = headers.ContainsKey("NServiceBus.ProcessingMachine") ? headers["NServiceBus.ProcessingMachine"] : "";
+
+                    var payloadXDoc = XDocument.Parse(payload);
+                    DateTime eventDate =
+                        DateTime.Parse(message.Descendants().First(d => d.Name.LocalName == "EventDateTime").Value);
+                    string firstMessageId =
+                        payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "MessageID")?.Value;
+
+                    string locationid = string.Empty;
+                    string pon = string.Empty;
+                    string messageNote = string.Empty;
+                    string payloadType = string.Empty;
+                    string deadletterAck = doc.Root.Element("Class")?.Value;
+                    switch (messageType)
+                    {
+                        case "Mps.TeRx.Service.Command.RxFillCommand":
+                            payloadType = "RxFill";
+                            var notFilledElement = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "NotFilled");
+                            if (notFilledElement != null)
+                            {
+                                messageNote = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "Note")?.Value;
+                            }
+                            pon = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "PrescriberOrderNumber")?.Value;
+                            locationid = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "To")?.Value;
+                            break;
+                        case "Mps.TeRx.Service.Event.OutboundPharmacyMessage.OutboundPharmacyMessageEvent":
+                            payloadType = message.Descendants().FirstOrDefault(d => d.Name.LocalName == "MessageControlType")?.Value;
+                            pon = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "PrescriberOrderNumber")?.Value;
+                            locationid = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "From")?.Value;
+                            break;
+                        case "Mps.TeRx.Service.Command.PayloadCommand":
+                            deadletterAck = doc.Root.Element("Class")?.Value;
+                            payloadType = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "Body")?.Elements().FirstOrDefault()?.Name.LocalName ?? payloadXDoc.Root.Name.LocalName;
+                            pon = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "PrescriberOrderNumber")?.Value;
+                            locationid = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "FacilityID")?.Value;
+                            break;
+                        case "Mps.TeRx.Service.Command.OutboundPharmacyMessage.OutboundPharmacyMessageResponseQueuedForPersistenceCommand":
+                            messageNote = doc.Root.Element("Class")?.Value;
+                            payloadType = message.Descendants().FirstOrDefault(d => d.Name.LocalName == "MessageControlType")?.Value;
+                            pon = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "PrescriberOrderNumber")?.Value;
+                            locationid = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "From")?.Value;
+                            break;
+                        case "Mps.TeRx.Service.Command.OutboundPharmacyMessage.OutboundPharmacyMessageQueuedForEndpointDeliveryCommand":
+                            messageNote = doc.Root.Element("Class")?.Value;
+                            payloadType = message.Descendants().FirstOrDefault(d => d.Name.LocalName == "MessageControlType")?.Value;
+                            pon = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "PrescriberOrderNumber")?.Value;
+                            locationid = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "From")?.Value;
+                            break;
+                            // Todo: add parsing for notification messages??
+                        default:
+                            break;
+                    }
+
+                    csv.WriteField(eventDate);
+                    csv.WriteField(pon);
+                    csv.WriteField(locationid);
+                    csv.WriteField(messageType);
+                    csv.WriteField(payloadType);
+                    csv.WriteField(messageNote);
+                    csv.WriteField(deadletterAck);
+                    csv.WriteField(firstMessageId);
+                    csv.WriteField(payload.Length > 30000 ? payload.Remove(30000) : payload);
+                    csv.WriteField(stackTrace);
+                    csv.WriteField(file.FullName);
+                    csv.WriteField($"{headers["NServiceBus.OriginatingEndpoint"]}@{headers["NServiceBus.OriginatingMachine"]}");
+                    csv.WriteField($"{processingEndpoint}@{processingMachine}");
+                    csv.NextRecord();
                 }
-                if (doc.Root.Descendants().First().Name.LocalName == "BSSMPSRxConnectorServiceExceptionNotification")
+
+                var outputFileDialog = new SaveFileDialog()
                 {
-                    Console.WriteLine("Ignoring error sending notification email: " + file.FullName);
-                    continue;
-                }
-
-                var payload = doc.Descendants().FirstOrDefault(d => d.Name.LocalName == "Payload" | d.Name.LocalName == "MessagePayload")?.Value;
-                if (string.IsNullOrEmpty(payload))
+                    Title = "Select Output File",
+                    FileName = "Output.csv",
+                    AddExtension = true,
+                    Filter = "CSV|*.csv"
+                };
+                var outputFileResult = outputFileDialog.ShowDialog();
+                while (outputFileResult != DialogResult.OK)
                 {
-                    Console.WriteLine("Ignoring message with no payload: " + file.FullName);
-                    continue;
+                    if (outputFileResult == DialogResult.Cancel)
+                    {
+                        Console.WriteLine("Canceled. Press any key to exit.");
+                        Console.ReadKey();
+                        return;
+                    }
+                    outputFileResult = ofd.ShowDialog();
                 }
 
-                var payloadXDoc = XDocument.Parse(payload);
-                DateTime eventDate = DateTime.Parse(doc.Descendants().First(d => d.Name.LocalName == "EventDateTime").Value);
-                string firstMessageId = payloadXDoc.Descendants().First(d => d.Name.LocalName == "MessageID").Value;
-
-                string locationid = string.Empty;
-                string pon = string.Empty;
-                string payloadNote = string.Empty;
-                switch (messageName)
+                try
                 {
-                    case "RxFill Command":
-                        payloadNote = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "Note")?.Value;
-                        pon = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "PrescriberOrderNumber")?.Value;
-                        locationid = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "To")?.Value;
-                        break;
-                    case "MPSRx Connector Service Invalid NCPDP Schema Notification":
-                        payloadNote = doc.Descendants().FirstOrDefault(d => d.Name.LocalName == "ExceptionErrorMessage")?.Value;
-                        locationid = payloadXDoc.Descendants("Facility").Descendants("NPI").FirstOrDefault()?.Value;
-                        pon = doc.Descendants().FirstOrDefault(d => d.Name.LocalName == "OrderID")?.Value;
-                        break;
-                    case "OutboundPharmacyMessageEvent":
-                        payloadNote = doc.Descendants().First(d => d.Name.LocalName == "MessageControlType")?.Value;
-                        pon = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "PrescriberOrderNumber")?.Value;
-                        locationid = payloadXDoc.Descendants().FirstOrDefault(d => d.Name.LocalName == "From")?.Value;
-                        break;
+                    using (var streamWriter = new StreamWriter(outputFileDialog.FileName))
+                    {
+                        streamWriter.Write(exportStringBuilder);
+                    }
                 }
-
-                csvBuilder.AppendFormat("{0}\t{1}\t{2}\t{1}-{2}\t{3}\t{4}\t{5}\t{6}\t{7}\r\n", eventDate, pon, locationid, messageName, payloadNote, firstMessageId, payload, file.FullName);
+                catch (Exception ex)
+                {
+                    var regColor = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"Error saving output: {ex.Message}");
+                    Console.WriteLine("File output has been saved to your clipboard instead.");
+                    Clipboard.SetData(DataFormats.Text, exportStringBuilder.ToString());
+                    Console.ForegroundColor = regColor;
+                }
             }
 
-            Clipboard.SetData(DataFormats.StringFormat, csvBuilder.ToString());
-            Console.WriteLine("CSV Copied to Clipboard! Press any key to exit.");
+            Console.WriteLine("All done! Press any key to exit.");
             Console.ReadKey();
         }
     }
